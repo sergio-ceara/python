57146ba6429909517d0447bd64e59e63e3ac6a53
Sérgio Sousa <sergio_ceara@yahoo.com.br>
2024-08-23 22:30:31 -0300

    removendo arquivo 2 vez.

--- teste_23-08-2024_22-03.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import sys 
-import io
-
-sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
-
-print("Olá mundo Python!")
-print("Data: 23/08/2024")
-print("Horário: 22:26")
